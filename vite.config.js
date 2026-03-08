69288b4e4a394577e2ec28f3495bd382eec9d9f5
Sekharendu <sekharendudey12@gmail.com>
2026-03-08 19:26:31 +0530

    Implemented Tip Tap and Tailwind repos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Ensure this is installed if using v4
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
+  define: {
+    '__VERSION__': JSON.stringify('1.0.0'),
+  },
 })
